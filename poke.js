ee238783446f0ee94a5abfe400130d9ad670f363
Marc DELOMEZ <marc.delomez@free.fr>
2019-03-24 16:49:56 +0100

    add topic master.status

--- poke.js
+++ poke.js
@@ -6,7 +6,9 @@ var params = util.getParams()
 
 console.log('params', params)
 
-
+if (params.data != undefined) {
+	params.data = util.safeEval(params.data)
+}
 
 if (params.topic == undefined) {
 	console.log('Usage: node poke topic=topicName [data=data] [file=fileName]')
@@ -18,7 +20,7 @@ if (params.file != undefined) {
 	params.data = JSON.parse(content.toString())
 }
 
-//console.log('data', params.data)
+console.log('data', params.data)
 
 
 var agentName = 'poke.' + Date.now() % 100000
@@ -29,11 +31,9 @@ process.argv[2] = agentName
 const agent = require('./lib/agent')
 
 
-var hist = (params.hist === 'true')
-
 agent.onConnect(() => {
 	console.log('send', params.data)
-	agent.emit(params.topic, params.data)
+	agent.emitTopic(params.topic, params.data)
 	process.exit(0)	
 })
 
