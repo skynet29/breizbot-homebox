66ad764c6f3a1ca358c5c4445499010cf13682b4
Marc DELOMEZ <marc.delomez@free.fr>
2019-05-24 21:04:54 +0200

    update media agent

--- agents/media.js
+++ agents/media.js
@@ -12,7 +12,7 @@ agent.registerService('homebox.media.drive', function(req) {
 	console.log('req', req)
 
 	return drivelist.list().then((drives) => {
-		console.log('drives', drives)
+		//console.log('drives', drives)
 		return drives
 			.filter((d) => d.isUSB)
 			.map((d) => d.mountpoints[0].label)
@@ -26,12 +26,12 @@ agent.registerService('homebox.media.list', function(req) {
 
 	return drivelist.list()
 	.then((drives) => {
-		console.log('drives', drives)
+		//console.log('drives', drives)
 		return drives.find((d) => d.isUSB && d.mountpoints[0].label == driveName)
 
 	})
 	.then((drive) => {
-		console.log('drive', drive)
+		//console.log('drive', drive)
 		const mountPath = drive.mountpoints[0].path
 		const fullPath = path.join(mountPath, destPath)
 		console.log('fullPath', fullPath)
@@ -40,7 +40,7 @@ agent.registerService('homebox.media.list', function(req) {
 		})
 	})	
 	.then((info) => {
-		console.log('info', info)
+		//console.log('info', info)
 		const promises = info.files.map((file) => {
 			return fs.lstat(path.join(info.fullPath, file)).then((statInfo) => {	
 				return {
@@ -63,5 +63,64 @@ agent.registerService('homebox.media.list', function(req) {
 
 })
 
+agent.registerService('homebox.media.load', function(req) {
+	console.log('req', req)
+	const {driveName, fileName, range} = req
+
+    const positions = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(positions[0]);
+    console.log('start', start)
+    // if last byte position is not present then it is the last byte of the video file.
+    //const end = positions[1] ? parseInt(positions[1]) : total - 1;
+    //var chunksize = (end-start)+1;	
+
+	return drivelist.list()
+	.then((drives) => {
+		console.log('drives', drives)
+		return drives.find((d) => d.isUSB && d.mountpoints[0].label == driveName)
+
+	})
+	.then((drive) => {
+		console.log('drive', drive)
+		const mountPath = drive.mountpoints[0].path
+		const fullPath = path.join(mountPath, fileName)
+		
+		return fs.lstat(fullPath).then((statInfo) => {
+			return {size: statInfo.size, fullPath}
+		})
+	})	
+	.then((info) => {
+		console.log('info', info)
+		const {size, fullPath} = info
+		return fs.open(fullPath, 'r').then((fd) => {
+			return {fd, size}
+		})
+	})
+	.then((info) => {
+		console.log('info', info)
+		const {fd, size} = info
+		//const end = positions[1] ? parseInt(positions[1]) : statInfo.size - 1;
+
+		const chunkSize = 1024*1024
+		console.log('start', start)
+		return fs.read(fd, Buffer.alloc(chunkSize), 0, chunkSize, start).then((results) => {
+			return {fd, size, results}
+		})
+	})
+	.then((info) => {
+		console.log('info', info)
+		let {results, fd, size} = info
+		fs.close(fd)
+		console.log('results', results)
+		return {
+			size,
+			start,
+			bytesRead: results.bytesRead,
+			buffer: results.buffer.toString('base64')
+		}
+	})
+
+
+})
 
 agent.start()
