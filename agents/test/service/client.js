812a940fb0165d19243b94bbbb4390144ac7a4ee
Marc DELOMEZ <marc.delomez@free.fr>
2019-05-24 14:59:00 +0200

    reintegrate service broker support

--- agents/test/service/client.js
+++ agents/test/service/client.js
@@ -4,8 +4,8 @@ agent.onConnect(function() {
 	agent.callService('sum', {a: 10, b: 20}).then(function(resp) {
 		console.log('resp', resp)
 	})
-	.catch(function(statusCode) {
-		console.warn('statusCode', statusCode)
+	.catch(function(err) {
+		console.warn('err', err)
 	})
 })
 
